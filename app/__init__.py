1a52eacacd498b80f823979dbe9fc14551017671
Cristian Sousa <eng.cristiansousa@gmail.com>
2022-04-04 23:34:49 -0300

    Adicionadas rotas de teste e home

--- app/__init__.py
+++ app/__init__.py
@@ -1,15 +1,28 @@
 """System module."""
 __version__ = '0.1.0'
-from flask import Flask
+import os
+from flask import Flask, render_template
+from config import BASE_DIR
 
 
 def create_app():
     """
-    Esta função criar a instância do app
+    Esta função cria a instância do app
     :return:
     """
-    app = Flask(__name__)
+    template_folder = os.path.join(BASE_DIR, "resources", "templates")
+    static_folder = os.path.join(BASE_DIR, "resources", "templates")
+    app = Flask(__name__,
+                template_folder=template_folder,
+                static_folder=static_folder)
+
     @app.route("/testing")
     def testing():
-        return "Testing"
+        render = render_template("testing.html")
+        return render
+
+    @app.route("/home")
+    def home():
+        render = render_template("home.html")
+        return render
     return app
